5f5145c696e428b5ce0dbca98c0cf5f75dc66104
Akash Kumar <akash@BASCCORP.COM>
2026-02-24 13:44:18 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">app hellso world</div>;
+  return <div className="App">hello world am from india</div>;
 }
 
 export default App;
